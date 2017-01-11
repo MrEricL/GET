d8c12be6465179f677dc093fd9d37fa1b1f81629
MrEricL <eli6@stuy.edu>
2017-01-11 09:33:24 -0500

    new cs 1

--- blackjack.java
+++ blackjack.java
@@ -340,12 +340,12 @@ public class blackjack implements casinorules {
     // THE BOTS________________________________________________________________
 
     //EASY
-    public void ezbot(){
+    /*    public void ezbot(){
 	    if (jackDraw > 0){
 		    jack.add(
     }
 
-
+    */
 
     //MEDIUM
 
