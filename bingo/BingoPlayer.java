63ecdf29a7dc4137927aa6156d56a136af4ffd27
Gian Tricarico <gtricarico@stuy.edu>
2017-01-12 10:56:05 -0500

    Add class to extend Bingo

---
+++ bingo/BingoPlayer.java
@@ -0,0 +1,35 @@
+// Period 4
+// Team GET
+// Gian Tricarico, Eric Li, Truc Dao
+
+public class BingoPlayer {
+
+    // Variables for overloaded constructor
+    private int difficulty;
+    private double money;
+
+    // Declare player and opponents' bingo cards.
+    private int[][] p1card;
+    private int[][] opp1card;
+    private int[][] opp2card;
+
+    // Letter and number called by the bingo person
+    // (not sure what the official name is)
+    private int calledLetter; // is an int because method converts letter to number for indexing in the array
+    private int calledNumber;
+
+    // Overloaded constructor
+    public Bingo( diff, mon, b ) {
+	difficulty = diff;
+	money = mon;
+	bet = b;
+	fillCards()
+    }
+
+    // Fills cards with random numbers
+    public void fillCard(){
+    }
+
+    public static void main( String[] args ) {
+    }
+}
