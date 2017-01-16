62f3f2a39f404bcb3db86d9a9a5d1f0820d77b41
Gian Tricarico <gtricarico@stuy.edu>
2017-01-16 02:34:00 -0500

    Add setup for bingo cards, printing and number calling functionality

--- bingo/Bingo.java
+++ bingo/Bingo.java
@@ -9,38 +9,92 @@ public class Bingo {
     private int difficulty;
     private double bet;
 
-    private BingoPlayer player0, opp0, opp1, opp2, opp3, opp4, opp5;
+    private BingoPlayer player0, opp0, opp1, opp2, opp3, opp4, opp5, opp6, opp7;
 
     private ArrayList<BingoPlayer> players;
+    // The number of BingoPlayers added to players will depend
+    // on the difficulty setting, and operations will only be performed
+    // on the BingoPlayers in players.
 
-    // Letter and number called by the bingo person
-    // (not sure what the official name for the bingo person is)
-    private int calledLetter; // is an int because it is used for indexing into the card array
-    private int calledNumber;
+    private boolean bingo; // indicates whether a player has won
+    
+    // Letter and number called by the caller
+    private int letter; // is an int even though it represents a letter because it is used for indexing into the card array
+    private int number;
+
+    
 
     // Overloaded constructor
-    public Bingo( diff, b ) {
+    public Bingo( int diff, double b ) {
 	difficulty = diff;
 	bet = b;
+	start();
+    }
+    
+    public void start() {
 	players = new ArrayList<BingoPlayer>();
+	player0 = new BingoPlayer( difficulty );
+	opp0 = new BingoPlayer( difficulty );
+	opp1 = new BingoPlayer( difficulty );
 	players.add( player0 );
 	players.add( opp0 );
 	players.add( opp1 );
 	if ( difficulty > 1 ) {
+	    opp2 = new BingoPlayer( difficulty );
+	    opp3 = new BingoPlayer( difficulty );
+	    opp4 = new BingoPlayer( difficulty );
 	    players.add( opp2 );
 	    players.add( opp3 );
+	    players.add( opp4 );
 	}
 	if ( difficulty > 2 ) {
-	    players.add( opp4 );
+	    opp5 = new BingoPlayer( difficulty );
+	    opp6 = new BingoPlayer( difficulty );
+	    opp7 = new BingoPlayer( difficulty );
 	    players.add( opp5 );
+	    players.add( opp6 );
+	    players.add( opp7 );
 	}
-	// Instantiate only the BingoPlayers in players
-	for ( int i = 0; i < players.size(); i += 1 ) {
-	    players.get( i ) = new BingoPlayer( difficulty );
+	// System.out.println( players );
+    } // end start()
+
+    public double play() {
+	bingo = false;
+	while ( !bingo ) {
+	    System.out.println( "This is your card:\n\n" );
+	    System.out.println( player0.printableCard() );
+	    System.out.println( call() );
+	    bingo = true;
 	}
+	return bet;
     }
 
-    // Fills cards with random numbers
-    public void fillCard(){
+    public String call() {
+	int range;
+	String letters;
+	String retStr;
+	
+	// rand int in [0, 5)
+	letter = (int)( 5 * Math.random() );
+
+	range = 25 * difficulty;
+	// rand int in [1, range +1)
+	number = (int)( 1 + range * Math.random() );
+	
+	letters = "BINGO";
+	retStr = "Caller: ";
+	retStr += letters.substring( letter, letter + 1 );
+	retStr += number;
+	if ( ( letter == 0 ) && ( number == 4 ) ) {
+	    retStr += "\nEveryone: ...and after!";
+	}
+	return retStr;
+    }
+
+    // main method for testing purposes
+    public static void main( String[] args ) {
+	Bingo japajoe;
+	japajoe = new Bingo( 1, 50.0 );
+	japajoe.play();
     }
 }
--- bingo/BingoPlayer.java
+++ bingo/BingoPlayer.java
@@ -6,17 +6,43 @@ public class BingoPlayer {
 
     private int[][] card;
 
-    // Default constructor
-    public BingoPlayer() {
+    // Overloaded constructor
+    public BingoPlayer( int diff ) {
+	int range;
+	range = 25 * diff;
+	card = new int[5][5];
 	for ( int i = 0; i < card.length; i += 1 ) {
 	    for ( int x = 0; x < card[i].length; x += 1 ) {
-		card[i][x] = 1 + 24 * Math.random();
+		// The range of the ints in the card will be [1, range+1)
+		card[i][x] = (int)( 1 + range * Math.random() );
+	    }
+	}
     }
 
-    // Fills cards with random numbers
-    public void fillCard(){
+    // printableCard() returns printable version of bingo card
+    public String printableCard() {
+	String retStr;
+	retStr = "B   I   N   G   O\n\n";
+	for ( int[] x : card ) {
+	    for ( int y : x ) {
+		retStr += y;
+		// space out columns
+		if ( y < 10) {
+		    retStr += "   ";
+		}
+		else {
+		    retStr += "  ";
+		}
+	    }
+	    retStr += "\n\n";
+	}
+	return retStr;
     }
 
+    // main method for testing purposes
     public static void main( String[] args ) {
+	BingoPlayer jebediah;
+	jebediah = new BingoPlayer( 1 );
+	System.out.println( jebediah.printableCard() );
     }
 }
