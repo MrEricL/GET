973da93126b5fb0f17ac2879b02c6f087012a765
tructdao <tdao@stuy.edu>
2017-01-17 09:55:23 -0500

    slots

--- SlotsPics/Pictures.java
+++ SlotsPics/Pictures.java
@@ -7,8 +7,18 @@ public class Pictures {
 	return ret;
     }
 
+    public String apple(){
+	String ret = "    /    \n" +
+	             "  ---- \n" +
+	             " /    \\ \n"+
+	            "|      |  \n" +
+	    " \\____/ " ;
+	return ret;
+    }
+
     public static void main(String[] args){
 	Pictures p = new Pictures();
 	System.out.println(p.crown());
+	System.out.println(p.apple());
     }
 }
